90652f4e64a5b1a052de8bd73056ee868bd050e3
Patrick Todd <patrick.a.todd@gmail.com>
2017-09-04 17:52:39 -0400

    altered flask api to flask_api for more consistent naming conventions

--- flask_api/server.py
+++ flask_api/server.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import plaid
+import plaid_config
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -8,20 +9,16 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+PLAID_CLIENT_ID = plaid_config.CLIENT_ID
+PLAID_SECRET = plaid_config.SECRET
+PLAID_PUBLIC_KEY = plaid_config.PUBLIC_KEY
+PLAID_ENV = plaid_config.ENV
 
-PLAID_CLIENT_ID = '59ab3519bdc6a43c31e518a3'
-PLAID_SECRET = '342e5fcbe9ea7dbc71097d4f54c568'
-PLAID_PUBLIC_KEY = '99d40644c2dfd609a4e9378c9fdd47'
-PLAID_ENV ='sandbox'
 
-@app.route("/")
+@app.route("/api")
 def index():
     print "The server is running. You'd better catch it!"
     return render_template('index.ejs')
-@app.route("/page")
-def page():
-    return render_template('page.ejs')
-
 
 if __name__ == "__main__":
     app.run()
