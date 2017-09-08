4686090619b510ee1fa609a25f37e807ae91b63d
Patrick Todd <patrick.a.todd@gmail.com>
2017-09-07 22:01:54 -0400

    modified flask server to work with the request response

--- flask_api/server.py
+++ flask_api/server.py
@@ -1,13 +1,15 @@
 import os
 import datetime
 import plaid
-from plaid_config import CLIENT_ID, ENV, PUBLIC_KEY, SECRET
+import plaid_config
+import requests
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import jsonify
+from flask_api import FlaskAPI
 
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 
 PLAID_CLIENT_ID = plaid_config.CLIENT_ID
 PLAID_SECRET = plaid_config.SECRET
@@ -17,7 +19,9 @@ PLAID_ENV = plaid_config.ENV
 
 @app.route("/api")
 def index():
+    r = requests.get("https://www.reddit.com/r/spaceporn/.json?")
     print "The server is running. You'd better catch it!"
+    return r.json()
 
 if __name__ == "__main__":
     app.run()
